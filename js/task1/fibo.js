9ae4d615abee725d771b8ed43e5a04c584bf3638
Eugen Kondratiev <kondratiev.es@gmail.com>
2019-12-17 01:21:56 +0200

    js-task1 simple straite fibo visualisation

--- js/task1/fibo.js
+++ js/task1/fibo.js
@@ -3,7 +3,7 @@ function* fiboGenerator() {
     let i1 = 1;
     let sign;
     while (true) {
-        console.log(i, i1);
+        console.log("i="+i, "i1="+i1);
        if (sign) {
         nextFibo = i+i1;
        } else {
@@ -23,17 +23,38 @@ const fibo = fiboGenerator();
 
 
 function getSomeFibo(n, sign) {
-    for(let i=0;i<n;i++) console.log(i, fibo.next(sign));
+    for(let i=0;i<n;i++) console.log(i, fibo.next(sign).value);
 }
 
-console.log(/**========= */);
-getSomeFibo(5, true);
+function getFiboString(n, sign) {
+    const resp =[];
+    for(let i=0;i<n;i++)  resp.push(fibo.next(sign).value);
+    return resp.join(",")
+}
 
-console.log(fibo.next(true));
-console.log(fibo.next(true));
-console.log(fibo.next(true));
-console.log(fibo.next(true));
-console.log(fibo.next(false));
-console.log(fibo.next(false));
+// console.log(/**========= */);
+// getSomeFibo(5, true);
+
+// console.log(fibo.next(true));
+// console.log(fibo.next(true));
+// console.log(fibo.next(true));
+// console.log(fibo.next(true));
+// console.log(fibo.next(false));
+// console.log(fibo.next(false));
+// getSomeFibo(5, false);
+const textFrame = document.getElementsByClassName("text-frame")[0];
 
 
+document.getElementById("more").addEventListener("click",function() {
+    const newDiv = document.createElement("div");
+    newDiv.textContent = getFiboString(parseInt(prompt("Сколько числе желаете сгенерировать", 3)),true); 
+    textFrame.appendChild(newDiv)    
+})
+
+
+
+document.getElementById("less").addEventListener("click", function() {
+    const newDiv = document.createElement("div");
+    newDiv.textContent = getFiboString(parseInt(prompt("Сколько числе желаете сгенерировать", 3)),false); 
+    textFrame.appendChild(newDiv)    
+})
