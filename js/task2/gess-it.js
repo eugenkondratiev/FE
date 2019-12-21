22ccc72333c103e7b30658f36dd20f4c81068bb0
Eugen Kondratiev <kondratiev.es@gmail.com>
2019-12-21 21:46:09 +0200

    js task2 console version done

--- js/task2/gess-it.js
+++ js/task2/gess-it.js
@@ -1,13 +1,16 @@
-function* guessGame(MAX_NUM) {
+function guessGame(MAX_NUM) {
     MAX_NUM = MAX_NUM || 100;
-    let data = {
-        limits: [0, MAX_NUM]
-    };
+    // let data = {
+    //     limits: [0, MAX_NUM]
+    // };
     const theNumber = Math.floor(Math.random() * MAX_NUM);
     let attempts = 0;
     userChoise = -1;
-    const makeMove = (d) => {
-        attempts++;
+    const makeMove = (data) => {
+        
+        userChoise = data.userChoise !== undefined ?  parseInt(data.userChoise) : userChoise;
+        const d = {attempts : ++attempts};
+
         if (userChoise === theNumber) {
             d.success = true;
             d.attempts = attempts;
@@ -18,22 +21,27 @@ function* guessGame(MAX_NUM) {
     };
     // const getData = () => { data };
     // const askUser = (data)
-    for (; userChoise !== theNumber;) {
-        const askUser = parseInt(yield data);
-        userChoise = askUser && askUser > data.limits[0] ? askUser : data.limits[0];
-        data = makeMove(data);
-        console.log("data -" + data, "userChoise -" + userChoise);
-    }
+    // for (; userChoise !== theNumber;) {
+    //     const amswer = parseInt(yield data);
+    //     userChoise = askUser && askUser > data.limits[0] ? askUser : data.limits[0];
+    //     data = makeMove(data);
+    //     console.log("data -" + data, "userChoise -" + userChoise);
+    // }
     // return getData;
+    return makeMove;
 };
 
 
-let g = guessGame(100);
-let currentData = { limits: [0, 100] };
-console.log("currentData.limits " + currentData.limits, currentData.success);
+const MAX = 100;
+let g = guessGame(MAX);
+let currentData = { limits: [0, MAX] };
+console.log("currentData.limits " + currentData.limits, "currentData.success - "+currentData.success);
+
 
 while (currentData.success == undefined) {
-    currentData = g.next(prompt(`выберите число от ${currentData.limits[0]} до ${currentData.limits[1]}`, currentData.limits[1])).value;
+    // currentData = g.next(prompt(`выберите число от ${currentData.limits[0]} до ${currentData.limits[1]}`, currentData.limits[1])).value;
+    currentData.userChoise = prompt(`выберите число от ${currentData.limits[0]} до ${currentData.limits[1]}`, currentData.limits[1]);
+    currentData = g(currentData);
     console.log("currentData.limits -", currentData);
 }
 console.log(`Угадано за ${currentData.attempts} попыток!`);
